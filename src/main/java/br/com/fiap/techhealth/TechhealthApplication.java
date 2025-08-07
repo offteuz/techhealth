2c0264a2fe627e60196f86af5c952d54a55c541f
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-08-07 20:58:42 -0300

    First commit

---
+++ src/main/java/br/com/fiap/techhealth/TechhealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techhealth/TechhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
