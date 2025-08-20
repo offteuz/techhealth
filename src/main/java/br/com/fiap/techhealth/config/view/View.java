299a95e78ea51cfd7fbd3a5ef93591ffda8fae73
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-08-19 22:50:14 -0300

    build: Criacao da classe responsável pela anotacao e configuracao do @JsonView

---
+++ src/main/java/br/com/fiap/techhealth/config/view/View.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techhealth.config.view;
+
+public class View {
+
+    public static class Summary{}
+
+    public static class Synthetic extends Summary{}
+
+    public static class Analytic extends Synthetic{}
+
+    public static class Internal{}
+
+}
