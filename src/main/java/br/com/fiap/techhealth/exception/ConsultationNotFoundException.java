7cadd84a10bde742c576aab4eea27b94862b2c0c
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-08-28 22:31:59 -0300

    feat: Criacao da excecao para consultas não encontradas

---
+++ src/main/java/br/com/fiap/techhealth/exception/ConsultationNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.fiap.techhealth.exception;
+
+public class ConsultationNotFoundException extends RuntimeException{
+
+    public ConsultationNotFoundException(String message) {
+        super(message);
+    }
+
+    public ConsultationNotFoundException() {
+        super("Consulta não encontrada. Verifique!");
+    }
+}
